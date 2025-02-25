b7cc1632ac9204ce83057777992cf994e47cbc86
jaimeduffy <jaidufpan@alum.us.es>
2025-02-25 12:37:35 +0100

    cambios

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Q pasa primo");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 16078;
+
+app.get("/", (request, response )=>{
+    response.send("Hello from the server");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+});
+
+console.log(`Finish setup`)
